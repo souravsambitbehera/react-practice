7ecc59ec47b35a880e6077552a5b1b1615b0a62c
sourav sambit behera <souravsambit12@gmail.com>
2023-05-05 22:53:41 +0530

    component

---
+++ src/Practice/ShoppingCart.js
@@ -0,0 +1,85 @@
+import { useState } from "react";
+
+const initialProducts = [
+  {
+    id: 0,
+    name: "Baklava",
+    count: 1,
+  },
+  {
+    id: 1,
+    name: "Cheese",
+    count: 5,
+  },
+  {
+    id: 2,
+    name: "Spaghetti",
+    count: 2,
+  },
+];
+
+export default function ShoppingCart() {
+  const [products, setProducts] = useState(initialProducts);
+
+  function handleIncreaseClick(productId) {
+    setProducts(
+      products.map((product) => {
+        if (product.id === productId) {
+          return {
+            ...product,
+            count: product.count + 1,
+          };
+        } else {
+          return product;
+        }
+      })
+    );
+  }
+  function handleDecreaseClick(productId) {
+    let nextProducts = products.map(product => {
+      if (product.id === productId) {
+        return {
+          ...product,
+          count: product.count - 1
+        };
+      } else {
+        return product;
+      }
+    });
+    nextProducts = nextProducts.filter(p =>
+      p.count > 0
+    );
+    setProducts(nextProducts)
+  }
+  function handelDeleteClick(productId){
+    setProducts(
+      products.filter(product=>product.id !== productId)
+    )
+  }
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          {product.name} (<b>{product.count}</b>)
+          <button
+            onClick={() => {
+              handleIncreaseClick(product.id);
+            }}
+          >
+            +
+          </button> 
+          <button
+            onClick={() => {
+              handleDecreaseClick(product.id);
+            }}
+          >
+            –
+          </button>
+          <button onClick={()=>{handelDeleteClick(product.id)}}>
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
